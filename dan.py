280850cabb28d53071072007d94fd5465ece286e
xrc <tryitandsee@angrysock.com>
2020-08-29 23:23:37 -0500

    feat: add factory_boy to simulate api response faster

--- dan.py
+++ dan.py
@@ -10,15 +10,16 @@ class Post:
         self.__dict__ = data
 
 
-url = os.getenv("BOORU_URL")
-url_bits = urlparse(url)
-res = requests.get(
-    f"{url_bits.scheme}://{url_bits.hostname}/posts.json",
-    auth=(url_bits.username, url_bits.password),
-    params={"order": f"ordfav:{url_bits.username}"},
-)
-if not res.ok:
+if __name__ == '__main__':
+    url = os.getenv("BOORU_URL")
+    url_bits = urlparse(url)
+    res = requests.get(
+        f"{url_bits.scheme}://{url_bits.hostname}/posts.json",
+        auth=(url_bits.username, url_bits.password),
+        params={"order": f"ordfav:{url_bits.username}"},
+    )
+    if not res.ok:
+        pprint(res.json())
+
+
     pprint(res.json())
-
-
-pprint(res.json())
--- dan_test.py
+++ dan_test.py
@@ -1,2 +1,31 @@
+import factory
+
+import dan
+
+
+class PostFactory(factory.Factory):
+    class Meta:
+        model = dan.Post
+
+    updated_at = "2020-08-11T23:55:27.535-04:00"
+    large_file_url = "https://example.com/data/5e00fe186c8ce5b6ec406c743644367b.png"
+    file_url = "https://example.com/data/5e00fe186c8ce5b6ec406c743644367b.png"
+    file_size = 285415
+    file_ext = "png"
+    fav_count = 1
+    tag_count = 1
+    tag_count_artist = 0
+    tag_count_character = 0
+    tag_count_copyright = 0
+    tag_count_general = 0
+    tag_count_meta = 1
+    tag_string = "tagme"
+    tag_string_artist = ""
+    tag_string_character = ""
+    tag_string_copyright = ""
+    tag_string_general = ""
+    tag_string_meta = "tagme"
+
+
 def test_hmm():
     assert False
